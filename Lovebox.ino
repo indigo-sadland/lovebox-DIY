f0b4b9059a6bb053d089484e7304815cad2274ed
indigo-sadland <37074372+indigo-sadland@users.noreply.github.com>
2023-06-22 10:44:41 +0300

    Update Lovebox.ino

--- Lovebox.ino
+++ Lovebox.ino
@@ -1,28 +1,105 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
 #include <EEPROM.h>
-#include <Servo.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include "SSD1306Wire.h"
-
+//#include <WiFiClient.h>
+#include <Servo.h>
 #include "credentials.h"
+
+SSD1306Wire oled(0x3C, D2, D1); // ports to which oled is connected
+Servo myservo; 
+
+int pos = 90;
+int increment = -1;
+int analogPin = 0;     // port to which light sensor is connected
+int analogPinVal = 0;  
+
+String messageMode;
+String id;
+String msg;
+char idSaved; 
+bool wasRead;
+int lightValue;
+
 const char* ssid = _ssid;
 const char* password = _password;
 const String url = _url;
 
-SSD1306Wire oled(0x3C, D2, D1);
-Servo myservo; 
-int pos = 90;
-int increment = -1;
-int lightValue;
-String line;
-String messageMode;
-char idSaved; 
-bool wasRead;  
+void wifiConnect() {
+  if (WiFi.status() != WL_CONNECTED) {
+    //WiFi.hostname("LoveBox");
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Connecting");
+      Serial.println(WiFi.status());
+      delay(500);
+    }
+
+    Serial.println("WiFi connected");
+ 
+    // Print the IP address
+    Serial.print("IP address: ");
+    Serial.print(WiFi.localIP());
+  }
+}
+
+void getGistMessage() {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClientSecure client;
+    client.setInsecure();
+
+    HTTPClient https;
+    Serial.println("");
+    Serial.println("Requesting " + url);
+
+    if (https.begin(client, url)) {
+      int httpCode = https.GET();
+      //Serial.print("Response code: " + httpCode);
+
+      if (httpCode > 0) {
+        int i = 0;
+        // Read HTTP response line by line.
+        while(client.available()) {
+          if(i == 0) {
+            id = client.readStringUntil('\n');
+            i++;
+          } else if(i == 1) {
+            messageMode = client.readStringUntil('\n');
+            i++;
+          } else if(i == 2){
+            msg = client.readStringUntil('\n');
+            break;
+          } 
+        }
+        Serial.println(id);
+        Serial.println(messageMode);
+        Serial.println(msg);
+        //Serial.println(https.getString());
+      }
+      https.end();
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
+    
+    if(id[0] != idSaved){ // new message
+      wasRead = 0;
+      idSaved = id[0];
+      EEPROM.write(142, idSaved);
+      EEPROM.write(144, wasRead);
+      EEPROM.commit();
+
+      drawMessage(msg);
+    }
+  }
+}
 
 void drawMessage(const String& message) {
   oled.clear();
 
   // differentiat between 't'ext and image message
+  // TODO: CURRENTLY ONLY TEXT TYPE WORKS PROPERLY
   if(messageMode[0] == 't'){
     oled.drawStringMaxWidth(0, 0, 128, message);    
   } 
@@ -39,67 +116,6 @@ void drawMessage(const String& message) {
   oled.display();
 }
 
-void wifiConnect() {
-  if (WiFi.status() != WL_CONNECTED) {
-    WiFi.begin(ssid, password);
-
-    // connecting to WiFi
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-    }
-  }
-}
-
-void getGistMessage() {
-  const int httpsPort = 443;
-  const char* host = "gist.githubusercontent.com";
-  const char fingerprint[] = "70 94 DE DD E6 C4 69 48 3A 92 70 A1 48 56 78 2D 18 64 E0 B7";
-  
-  WiFiClientSecure client;
-  client.setFingerprint(fingerprint);
-  if (!client.connect(host, httpsPort)) {
-    return; // failed to connect
-  }
-  
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "User-Agent: ESP8266\r\n" +
-               "Connection: close\r\n\r\n");
-
-  while (client.connected()) {
-    String temp = client.readStringUntil('\n');
-    if (temp == "\r") {
-      break;
-    }
-  }
-  String id = client.readStringUntil('\n'); 
-  if(id[0] != idSaved){ // new message
-    messageMode = client.readStringUntil('\n');
-    if (messageMode[0] == 't'){
-      line = client.readStringUntil(0);
-    } else {
-      // binary image is corrupted if readStringUntil() takes too long
-      // fix: read string line by line
-      line = "";
-      for (int i = 0; i < 64; i++)     
-      {
-        line += client.readStringUntil('\n');
-        line += "\n";
-      }
-      if (line.length() != 8256)
-      {
-        getGistMessage();
-      }
-    }
-    wasRead = 0;
-    idSaved = id[0];
-    EEPROM.write(142, idSaved);
-    EEPROM.write(144, wasRead);
-    EEPROM.commit(); 
-    drawMessage(line);
-  }
-}
-
 void spinServo(){
     myservo.write(pos);      
     delay(50);    // wait 50ms to turn servo
@@ -110,7 +126,10 @@ void spinServo(){
     pos += increment;
 }
 
-void setup() {
+void setup()
+{
+  Serial.begin(9600);        // serial connect (for debug).
+
   myservo.attach(16);       // Servo on D0
   
   oled.init();
@@ -122,15 +141,18 @@ void setup() {
   oled.clear();
   oled.drawString(30, 30, "<3 LOVEBOX <3");
   oled.display();
-  
+
+  // Wi-Fi connect.
   wifiConnect();
 
   EEPROM.begin(512);
   idSaved = EEPROM.get(142, idSaved);
   wasRead = EEPROM.get(144, wasRead);
+
 }
 
-void loop() {
+void loop()
+{
   if (WiFi.status() != WL_CONNECTED) {
     wifiConnect();
   }
@@ -142,9 +164,13 @@ void loop() {
   while(!wasRead){   
     yield();
     spinServo();    // turn heart
-    lightValue = analogRead(0);      // read light value
-    if(lightValue > 300) { 
-      wasRead = 1;
+
+    
+    analogPinVal = analogRead(analogPin);     // read light value
+    //Serial.println(analogPinVal);
+
+    if(analogPinVal > 100) { 
+      wasRead = true;
       EEPROM.write(144, wasRead);
       EEPROM.commit();
     }
